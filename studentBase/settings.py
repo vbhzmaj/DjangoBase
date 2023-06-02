635168f04daf816123bb2e00b4f25bd39318caf5
vdraganovic <vdraganovic@gmail.com>
2023-06-03 00:10:02 +0200

    use bootstrap as a static resource

--- studentBase/settings.py
+++ studentBase/settings.py
@@ -115,7 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+#Static file configuration
 STATIC_URL = 'static/'
+STATIC_ROOT=BASE_DIR /'static'
+STATICFILES_DIRS=[
+    'studentBase/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
