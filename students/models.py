30ef5a69799efb69766f1093166b8995d6920bc1
vdraganovic <vdraganovic@gmail.com>
2023-06-03 15:07:25 +0200

    create student model and migrate

--- students/models.py
+++ students/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+
+    gender_choice = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+
+    roll_no = models.IntegerField()
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    full_name = models.CharField(max_length=100)
+    course = models.CharField(max_length=100)
+    grade = models.CharField(max_length=100)
+    gender = models.CharField(choices=gender_choice, max_length=100)
+
