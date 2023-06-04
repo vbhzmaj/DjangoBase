ed1bcb4c2c5bbeb630213314dcf2ec1313d6685c
vdraganovic <vdraganovic@gmail.com>
2023-06-04 11:45:12 +0200

    modify media files configuration

--- studentBase/settings.py
+++ studentBase/settings.py
@@ -124,8 +124,8 @@ STATICFILES_DIRS=[
 ]
 
 #Media files configuration
-MEDIA_ROOT=BASE_DIR/'media'
-MEDIA_URL='media/'
+MEDIA_ROOT=BASE_DIR 
+MEDIA_URL='/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
